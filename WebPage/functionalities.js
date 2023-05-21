bab36af63c3908810a1caf7493a86847c469b98b
sumitadep002 <adepsumit123@gmail.com>
2023-05-21 15:42:21 +0530

    voltage and battery rating functionalities

--- WebPage/functionalities.js
+++ WebPage/functionalities.js
@@ -1,42 +1,41 @@
-document.getElementById('led1').addEventListener('click', function() {
-    var led1 = document.getElementById('led1');
-
-    if (led1.classList.contains('led-green')) {
-        led1.classList.remove('led-green');
-        led1.classList.add('led-red');
-
-        // Send request with value 13 to the server
-        sendRequest('http://10.132.238.47:1234', 11);
-    } else {
-        led1.classList.remove('led-red');
-        led1.classList.add('led-green');
-
-        // Send request with value 12 to the server
-        sendRequest('http://10.132.238.47:1234', 12);
-        console.log("Hello")
-    }
-});
-
-document.getElementById('led2').addEventListener('click', function() {
-    var led2 = document.getElementById('led2');
-
-    if (led2.classList.contains('led-green')) {
-        led2.classList.remove('led-green');
-        led2.classList.add('led-red');
-
-        // Send request with value 14 to the server
-        sendRequest('http://127.0.0.1:5500', 13);
-    } else {
-        led2.classList.remove('led-red');
-        led2.classList.add('led-green');
-
-        // Send request with value 14 to the server
-        sendRequest('http://127.0.0.1:5500', 14);
-    }
-});
-
-function sendRequest(url, value) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('GET', url + '?value=' + value, true);
-    xhttp.send();
+// Function to update the LED status on the web page
+function updateLEDStatus(led, status) {
+    const statusElement = document.getElementById(led + 'Status');
+    statusElement.textContent = 'Status: ' + status;
 }
+
+// Function to toggle the state of an LED
+function toggleLED(led) {
+    fetch('/toggle?led=' + led)
+        .then(response => response.text())
+        .then(data => updateLEDStatus(led, data))
+        .catch(error => console.error('Error toggling LED:', error));
+}
+
+// Function to update the voltage value on the web page
+function updateVoltage(value) {
+    document.getElementById('voltageValue').textContent = value;
+}
+
+// Function to update the temperature value on the web page
+function updateTemperature(value) {
+    document.getElementById('temperatureValue').textContent = value;
+}
+
+// Function to fetch the real-time ratings data from the server
+function fetchRatingsData() {
+    // Fetch voltage rating
+    fetch('/voltage')
+        .then(response => response.text())
+        .then(data => updateVoltage(data))
+        .catch(error => console.error('Error fetching voltage rating:', error));
+
+    // Fetch temperature rating
+    fetch('/temperature')
+        .then(response => response.text())
+        .then(data => updateTemperature(data))
+        .catch(error => console.error('Error fetching temperature rating:', error));
+}
+
+// Periodically fetch real-time ratings data
+setInterval(fetchRatingsData, 1000); // Update every 1 second
